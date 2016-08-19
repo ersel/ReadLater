d649ff9dddb3579c9ad4cb07b5c446ad76e9d0f1
Ersel Aker <erselaker@gmail.com>
2016-08-19 22:45:27 +0100

    updates to get this little baby over to Chrome Web Store!

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,41 +1,38 @@
 'use strict';
 
-function addBookmark(parentFolder){
+function addBookmark(parentFolder) {
 	// Do NOT forget that the method is ASYNCHRONOUS
 	chrome.tabs.query({
-			active: true,               // Select active tabs
-			lastFocusedWindow: true     // In the current window
-	}, function(array_of_Tabs) {
+		active: true, // Select active tabs
+		lastFocusedWindow: true // In the current window
+	}, function (array_of_Tabs) {
 		// Since there can only be one active tab in one active window,
 		//  the array has only one element
 		var tab = array_of_Tabs[0];
 		// ... do something with url variable
-		chrome.bookmarks.search(tab.url,
-			function(BookmarksArray){
-				var bookmark = {title: tab.title, url: tab.url, parentId: parentFolder.id};
-				if(BookmarksArray.length > 0){
-					if(BookmarksArray[0].parentId !== parentFolder.id){
-						chrome.bookmarks.create(bookmark, window.close);
-					}
-				} else {
+		chrome.bookmarks.search(tab.url, function (BookmarksArray) {
+			var bookmark = { title: tab.title, url: tab.url, parentId: parentFolder.id };
+			if (BookmarksArray.length > 0) {
+				if (BookmarksArray[0].parentId !== parentFolder.id) {
 					chrome.bookmarks.create(bookmark, window.close);
 				}
+			} else {
+				chrome.bookmarks.create(bookmark, window.close);
 			}
-		);
+		});
 	});
-}
+};
 
 // MAIN FUNCTION
-chrome.bookmarks.search('ReadNowBookmarks',
-	function(BookmarksArray){
-		if(BookmarksArray.length > 0){
-			addBookmark(BookmarksArray[0]);
-		} else {
-			//Bookmark folder does not exist
-			var bookmark = {title: 'ReadNowBookmarks'};
-			chrome.bookmarks.create(bookmark, function(createdFolder){
-				addBookmark(createdFolder, window.close);
-			});
-		}
+chrome.bookmarks.search('ReadNowBookmarks', function (BookmarksArray) {
+	if (BookmarksArray.length > 0) {
+		addBookmark(BookmarksArray[0]);
+	} else {
+		//Bookmark folder does not exist
+		var bookmark = { title: 'ReadNowBookmarks' };
+		chrome.bookmarks.create(bookmark, function (createdFolder) {
+			addBookmark(createdFolder, window.close);
+		});
 	}
-);
+});
+//# sourceMappingURL=popup.js.map
